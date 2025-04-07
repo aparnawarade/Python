9c078ab0d86f8be0f7c0bf3ad6e0ab1fdb4a1b66
Aparna Warade <aparna.warade@outlook.com>
2025-04-07 14:21:07 -0400

    Update usingLisnkedList.py

--- DataStructure/Stack/usingLisnkedList.py
+++ DataStructure/Stack/usingLisnkedList.py
@@ -33,9 +33,58 @@ class Stack:
             print(temp.value)
             temp=temp.next
         
+"""
+Implement the push method for the Stack class that adds a new node with a given value to the top of the stack.
+
+The method should perform the following tasks:
+
+Create a new instance of the Node class using the provided value.
+
+Set the next attribute of the new node to point to the current top node.
+
+Update the top attribute of the Stack class to point to the new node.
+
+Increment the height attribute of the Stack class by 1.
+"""
+### WRITE PUSH METHOD HERE ###
+    #                            #
+    #                            #
+    #                            #
+    #                            #
+    ##############################
+    def push(self,value):
+        new_node=Node(value)
+        
+        if not self.top:
+            self.top = new_node
+            self.height = 1
+        else:
+            new_node.next=self.top
+            self.top=new_node
+            self.height +=1 
+            
+### WRITE POP METHOD HERE ###
+    #                           #
+    #                           #
+    #                           #
+    #                           #
+    #############################
+    def pop(self):
+        if not self.top:
+            return None
+        
+        temp=self.top
+        self.top=self.top.next
+        temp.next=None
+        self.height -=1 
+        return temp
+        
 
 
-my_stack = Stack(4)
+
+
+
+my_stack = Stack(2)
 
 print('Top:', my_stack.top.value)
 print('Height:', my_stack.height)
@@ -49,3 +98,22 @@ print('Height:', my_stack.height)
     Height: 1
 
 """
+
+print('Stack before push(1):')
+my_stack.print_stack()
+
+my_stack.push(1)
+
+print('\nStack after push(1):')
+my_stack.print_stack()
+
+
+#---
+print('Stack before pop():')
+my_stack.print_stack()
+
+print('\nPopped node:')
+print(my_stack.pop().value)
+
+print('\nStack after pop():')
+my_stack.print_stack()
